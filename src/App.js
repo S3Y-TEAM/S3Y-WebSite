0d6fc7eb50e2d6d628198d58118ab745e9351a26
dina <dinaelhadidy966@gmail.com>
2024-05-19 11:39:31 +0300

    Fix navigation

--- src/App.js
+++ src/App.js
@@ -1,64 +1,49 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Routes, Route, Router } from "react-router-dom";
-import LandingPage from './components/Authentication/Landingpage.jsx';
-import Login from './components/Authentication/Login.jsx';
-import SignUp from'./components/Authentication/SignUp.jsx';
-import LoginSuccess from './components/Authentication/LoginSuccess.jsx';
-import SignupSuccess from'./components/Authentication/SignupSuccess.jsx';
-import Experiance from './components/Authentication/Experiance.jsx';
-import Uploadproject from './components/Authentication/Uploadproject.jsx';
-import UploadCertificate from './components/Authentication/UploadCertificates.jsx';
-import Links from './components/Authentication/Links.jsx';
-import NationalData from './components/Authentication/NationalData.jsx';
-import UploadEmail from'./components/Authentication/UploadEmail.jsx';
-import UploadPhone from'./components/Authentication/UploadPhone.jsx';
-import ForgetPassword from './components/Authentication/ForgetPassword.jsx';
-import ChangePassword from './components/Authentication/ChangePassword.jsx';
+import LandingPage from "./components/Authentication/Landingpage.jsx";
+import Login from "./components/Authentication/Login.jsx";
+import SignUp from "./components/Authentication/SignUp.jsx";
+import LoginSuccess from "./components/Authentication/LoginSuccess.jsx";
+import SignupSuccess from "./components/Authentication/SignupSuccess.jsx";
+import Experiance from "./components/Authentication/Experiance.jsx";
+import Uploadproject from "./components/Authentication/Uploadproject.jsx";
+import UploadCertificate from "./components/Authentication/UploadCertificates.jsx";
+import Links from "./components/Authentication/Links.jsx";
+import NationalData from "./components/Authentication/NationalData.jsx";
+import UploadEmail from "./components/Authentication/UploadEmail.jsx";
+import UploadPhone from "./components/Authentication/UploadPhone.jsx";
+import ForgetPassword from "./components/Authentication/ForgetPassword.jsx";
+import ChangePassword from "./components/Authentication/ChangePassword.jsx";
 
-import Home from './components/DeveloperDashboard/Home.jsx';
-import Dashboard from './components/DeveloperDashboard/Home.jsx';
-import Aboutus from './components/DeveloperDashboard/Aboutus.jsx';
-import Rateus from './components/DeveloperDashboard/Rateus.jsx';
-import Contactus from './components/DeveloperDashboard/Contactus.jsx';
-import Setting from './components/DeveloperDashboard/Setting.jsx';
+import Home from "./components/DeveloperDashboard/Home.jsx";
+import Dashboard from "./components/DeveloperDashboard/Home.jsx";
+import Aboutus from "./components/DeveloperDashboard/Aboutus.jsx";
+import Rateus from "./components/DeveloperDashboard/Rateus.jsx";
+import Contactus from "./components/DeveloperDashboard/Contactus.jsx";
+import Setting from "./components/DeveloperDashboard/Setting.jsx";
 
 function App() {
   return (
     <div className="App">
-      
-       <Routes>
-       <Route path="/" element={<LandingPage/>} />
-       <Route path="/Login" element={<Login />} />
-       <Route path="/SignUp" element={<SignUp />} />
-       <Route path="/LoginSuccess" element={<LoginSuccess />} />
-       <Route path="/SignupSuccess" element={<SignupSuccess />} />
-       <Route path="/Experiance" element={<Experiance />} />
-       <Route path="/Uploadproject" element={<Uploadproject />} />
-       <Route path="/UploadCertificate" element={<UploadCertificate/>} />
-       <Route path="/Links" element={<Links/>} />
-       <Route path="/NationalData" element={<NationalData/>} />
-       <Route path="/UploadEmail" element={<UploadEmail/>} />
-       <Route path="/UploadPhone" element={<UploadPhone/>} />
-       <Route path="/ForgetPassword" element={<ForgetPassword />} />
-       <Route path="/ChangePassword" element={<ChangePassword/>} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/LoginSuccess" element={<LoginSuccess />} />
+        <Route path="/SignupSuccess" element={<SignupSuccess />} />
+        <Route path="/Experiance" element={<Experiance />} />
+        <Route path="/Uploadproject" element={<Uploadproject />} />
+        <Route path="/UploadCertificate" element={<UploadCertificate />} />
+        <Route path="/Links" element={<Links />} />
+        <Route path="/NationalData" element={<NationalData />} />
+        <Route path="/UploadEmail" element={<UploadEmail />} />
+        <Route path="/UploadPhone" element={<UploadPhone />} />
+        <Route path="/ForgetPassword" element={<ForgetPassword />} />
+        <Route path="/ChangePassword" element={<ChangePassword />} />
 
-       <Route path='/Home' element={<Home/>} />
-       <Route path='/Dashboard' element={<Dashboard/>} />
-       <Route path='/Aboutus' element={<Aboutus/>} />
-       <Route path='/Rateus' element={<Rateus/>} />
-       <Route path='/Contactus' element={<Contactus/>} />
-       <Route path='/Setting' element={<Setting/>} />
-
-
-
-
-
-       </Routes>
-      
-      
-     
-     
+        <Route path="/Home/*" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/components/DeveloperDashboard/Sidebar.jsx
+++ src/components/DeveloperDashboard/Sidebar.jsx
@@ -1,155 +1,226 @@
-import React from 'react';
-import { useState } from 'react';
-import'./Sidebar.css';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThLarge ,faListCheck ,faBell ,faComments
-         , faBriefcase, faUser
-         ,faBookmark, faGear
-        ,faPhone ,faPeopleGroup ,faStar
-        ,faSun, faMoon ,faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { useState } from "react";
+import "./Sidebar.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faThLarge,
+  faListCheck,
+  faBell,
+  faComments,
+  faBriefcase,
+  faUser,
+  faBookmark,
+  faGear,
+  faPhone,
+  faPeopleGroup,
+  faStar,
+  faSun,
+  faMoon,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
-
-        
-        
 const Sidebar = () => {
-  const [activeItem, setActiveItem] = useState('Dashboard');
-  
+  const [activeItem, setActiveItem] = useState("Dashboard");
+
   const handleItemClick = (item) => {
     setActiveItem(item);
-};
+  };
 
-    const [darkMode, setDarkMode] = useState(false);
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-        if (darkMode) {
-          document.body.classList.remove("dark");
-        } else {
-          document.body.classList.add("dark");
-        }
-      };
-      
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (darkMode) {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  };
 
-      const handleLogout = () => {
-        console.log('User logged out');
-    };
+  const handleLogout = () => {
+    console.log("User logged out");
+  };
 
   return (
-    <div className='sidebar'>
-     <img src={require("../images/logosidebar2.png")}/>
-     <hr className="sidebar-divider my-0" />
-     <ul>
-      <li className={`nav-item ${activeItem === 'Dashboard' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Dashboard" onClick={() => handleItemClick('Dashboard')}>
-                        <div className='selector1'>
-                            <span className='dashboard'>
-                                <FontAwesomeIcon icon={faThLarge} className='dashboardicon' />
-                                Dashboard
-                            </span>
-                        </div>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'Tasks' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Tasks" onClick={() => handleItemClick('Tasks')}>
-                        <span>
-                            <FontAwesomeIcon icon={faListCheck} className='dashboardicon' />
-                            Tasks
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'Notification' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Notification" onClick={() => handleItemClick('Notification')}>
-                        <span>
-                            <FontAwesomeIcon icon={faBell} className='dashboardicon' />
-                            Notification
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'Chat' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Chat" onClick={() => handleItemClick('Chat')}>
-                        <span>
-                            <FontAwesomeIcon icon={faComments} className='dashboardicon' />
-                            Chat
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'ApplayingJobs' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/ApplayingJobs" onClick={() => handleItemClick('ApplayingJobs')}>
-                        <span>
-                            <FontAwesomeIcon icon={faBriefcase} className='dashboardicon' />
-                            Applaying Jobs
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'Profile' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Profile" onClick={() => handleItemClick('Profile')}>
-                        <span>
-                            <FontAwesomeIcon icon={faUser} className='dashboardicon' />
-                            Profile
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'Bookmarks' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Bookmarks" onClick={() => handleItemClick('Bookmarks')}>
-                        <span>
-                            <FontAwesomeIcon icon={faBookmark} className='dashboardicon' />
-                            Bookmarks
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'Setting' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Setting" onClick={() => handleItemClick('Setting')}>
-                        <span>
-                            <FontAwesomeIcon icon={faGear} className='dashboardicon' />
-                            Setting
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'ContactUs' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Contactus" onClick={() => handleItemClick('ContactUs')}>
-                        <span>
-                            <FontAwesomeIcon icon={faPhone} className='dashboardicon' />
-                            Contact Us
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'AboutUs' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Aboutus" onClick={() => handleItemClick('AboutUs')}>
-                        <span>
-                            <FontAwesomeIcon icon={faPeopleGroup} className='dashboardicon' />
-                            About Us
-                        </span>
-                    </Link>
-                </li>
-                <li className={`nav-item ${activeItem === 'RateUs' ? 'active' : ''}`}>
-                    <Link className="nav-link" to="/Rateus" onClick={() => handleItemClick('RateUs')}>
-                        <span>
-                            <FontAwesomeIcon icon={faStar} className='dashboardicon' />
-                            Rate Us
-                        </span>
-                    </Link>
-                </li>
-     </ul>
-
-    
-              <div className='containsunmoon' onClick={toggleDarkMode}>
-                <div className={`togle ${!darkMode ? 'active' : ''}`}>
-                    <FontAwesomeIcon icon={faSun} className={`sun ${!darkMode ? 'active' : ''}`} />
-                </div>
-                <div className={`togle2 ${darkMode ? 'active' : ''}`}>
-                    <FontAwesomeIcon icon={faMoon} className={`moon ${darkMode ? 'active' : ''}`} />
-                </div>
+    <div className="sidebar">
+      <img src={require("../images/logosidebar2.png")} />
+      <hr className="sidebar-divider my-0" />
+      <ul>
+        <li
+          className={`nav-item ${activeItem === "Dashboard" ? "active" : ""}`}
+        >
+          <Link
+            className="nav-link"
+            to="/Home/Dashboard"
+            onClick={() => handleItemClick("Dashboard")}
+          >
+            <div className="selector1">
+              <span className="dashboard">
+                <FontAwesomeIcon icon={faThLarge} className="dashboardicon" />
+                Dashboard
+              </span>
             </div>
+          </Link>
+        </li>
+        <li className={`nav-item ${activeItem === "Tasks" ? "active" : ""}`}>
+          <Link
+            className="nav-link"
+            to="/Home/Tasks"
+            onClick={() => handleItemClick("Tasks")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faListCheck} className="dashboardicon" />
+              Tasks
+            </span>
+          </Link>
+        </li>
+        <li
+          className={`nav-item ${
+            activeItem === "Notification" ? "active" : ""
+          }`}
+        >
+          <Link
+            className="nav-link"
+            to="/Home/Notification"
+            onClick={() => handleItemClick("Notification")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faBell} className="dashboardicon" />
+              Notification
+            </span>
+          </Link>
+        </li>
+        <li className={`nav-item ${activeItem === "Chat" ? "active" : ""}`}>
+          <Link
+            className="nav-link"
+            to="/Home/Chat"
+            onClick={() => handleItemClick("Chat")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faComments} className="dashboardicon" />
+              Chat
+            </span>
+          </Link>
+        </li>
+        <li
+          className={`nav-item ${
+            activeItem === "ApplayingJobs" ? "active" : ""
+          }`}
+        >
+          <Link
+            className="nav-link"
+            to="/Home/ApplayingJobs"
+            onClick={() => handleItemClick("ApplayingJobs")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faBriefcase} className="dashboardicon" />
+              Applaying Jobs
+            </span>
+          </Link>
+        </li>
+        <li className={`nav-item ${activeItem === "Profile" ? "active" : ""}`}>
+          <Link
+            className="nav-link"
+            to="/Home/Profile"
+            onClick={() => handleItemClick("Profile")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faUser} className="dashboardicon" />
+              Profile
+            </span>
+          </Link>
+        </li>
+        <li
+          className={`nav-item ${activeItem === "Bookmarks" ? "active" : ""}`}
+        >
+          <Link
+            className="nav-link"
+            to="/Home/Bookmarks"
+            onClick={() => handleItemClick("Bookmarks")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faBookmark} className="dashboardicon" />
+              Bookmarks
+            </span>
+          </Link>
+        </li>
+        <li className={`nav-item ${activeItem === "Setting" ? "active" : ""}`}>
+          <Link
+            className="nav-link"
+            to="/Home/Setting"
+            onClick={() => handleItemClick("Setting")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faGear} className="dashboardicon" />
+              Setting
+            </span>
+          </Link>
+        </li>
+        <li
+          className={`nav-item ${activeItem === "ContactUs" ? "active" : ""}`}
+        >
+          <Link
+            className="nav-link"
+            to="/Home/Contactus"
+            onClick={() => handleItemClick("ContactUs")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faPhone} className="dashboardicon" />
+              Contact Us
+            </span>
+          </Link>
+        </li>
+        <li className={`nav-item ${activeItem === "AboutUs" ? "active" : ""}`}>
+          <Link
+            className="nav-link"
+            to="/Home/Aboutus"
+            onClick={() => handleItemClick("AboutUs")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faPeopleGroup} className="dashboardicon" />
+              About Us
+            </span>
+          </Link>
+        </li>
+        <li className={`nav-item ${activeItem === "RateUs" ? "active" : ""}`}>
+          <Link
+            className="nav-link"
+            to="/Home/Rateus"
+            onClick={() => handleItemClick("RateUs")}
+          >
+            <span>
+              <FontAwesomeIcon icon={faStar} className="dashboardicon" />
+              Rate Us
+            </span>
+          </Link>
+        </li>
+      </ul>
 
-     <button className='logout' onClick={handleLogout}>
-     <FontAwesomeIcon icon={faSignOutAlt} className='logouticon' onClick={handleLogout} />
-       Log out
-     </button>
+      <div className="containsunmoon" onClick={toggleDarkMode}>
+        <div className={`togle ${!darkMode ? "active" : ""}`}>
+          <FontAwesomeIcon
+            icon={faSun}
+            className={`sun ${!darkMode ? "active" : ""}`}
+          />
+        </div>
+        <div className={`togle2 ${darkMode ? "active" : ""}`}>
+          <FontAwesomeIcon
+            icon={faMoon}
+            className={`moon ${darkMode ? "active" : ""}`}
+          />
+        </div>
+      </div>
+
+      <button className="logout" onClick={handleLogout}>
+        <FontAwesomeIcon
+          icon={faSignOutAlt}
+          className="logouticon"
+          onClick={handleLogout}
+        />
+        Log out
+      </button>
     </div>
   );
 };
 
 export default Sidebar;
-
-
-
