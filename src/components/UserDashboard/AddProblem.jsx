c508aed3f498d856a5e6b5911bd463503538790c
dina <dinaelhadidy966@gmail.com>
2024-07-07 22:39:18 +0300

    Pass categories parent to PublishTask

--- src/components/UserDashboard/AddProblem.jsx
+++ src/components/UserDashboard/AddProblem.jsx
@@ -1,48 +1,51 @@
-import React from 'react';
-import { useState } from 'react';
-import './AddProblem.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import "./AddProblem.css";
+import { useNavigate } from "react-router-dom";
 
 const AddProblem = () => {
-    const [selected, setSelected] = useState(null);
-    const navigate = useNavigate();
+  const [selected, setSelected] = useState(null);
+  const navigate = useNavigate();
 
-    const handleButtonClick = (buttonType) => {
-      setSelected(buttonType);
-      if (buttonType === 'Developer'||buttonType === 'Worker') {
-        navigate('/UserHome/PublishTask'); 
-      } 
-    };
+  const handleButtonClick = (buttonType) => {
+    setSelected(buttonType);
+
+    if (buttonType === "developer" || buttonType === "worker") {
+      navigate("/UserHome/PublishTask", {
+        state: {
+          type: buttonType,
+        },
+      });
+    }
+  };
   return (
-    <div className='addproblem'>
-        <button className='backbutton'>
-            Back
-        </button>
-        <h3>Choose.. Which one will help you with your problem ?</h3>
-        <div className='chooseDW'>
-            <div className='dev'>
-            <img src={require("../images/tech.png")}/>
-            <div className='dev2'>
-            <button className={selected === 'Developer' ? 'selected' : ''} 
-              onClick={() => handleButtonClick('Developer')}>
-                Develper
+    <div className="addproblem">
+      <button className="backbutton">Back</button>
+      <h3>Choose.. Which one will help you with your problem ?</h3>
+      <div className="chooseDW">
+        <div className="dev">
+          <img src={require("../images/tech.png")} />
+          <div className="dev2">
+            <button
+              className={selected === "Developer" ? "selected" : ""}
+              onClick={() => handleButtonClick("developer")}
+            >
+              Develper
             </button>
-            </div>
-            </div>
-            <div className='worker'>
-            <img src={require("../images/worker 2.png")}/>
-            <div className='worker2'>
-            <button 
-            className={selected === 'Worker' ? 'selected' : ''} 
-            onClick={() => handleButtonClick('Worker')}>
-                Worker
-            </button>
-            </div>
-            </div>
+          </div>
         </div>
-       
-      
-
+        <div className="worker">
+          <img src={require("../images/worker 2.png")} />
+          <div className="worker2">
+            <button
+              className={selected === "Worker" ? "selected" : ""}
+              onClick={() => handleButtonClick("worker")}
+            >
+              Worker
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
