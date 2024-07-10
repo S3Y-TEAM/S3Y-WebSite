da370a84f134d6b252543117eb2e80c2d8ca220d
dina <dinaelhadidy966@gmail.com>
2024-07-10 20:02:41 +0300

    Make host url dynamic

--- src/utils/services.js
+++ src/utils/services.js
@@ -2,13 +2,11 @@ export const postRequest = async (url, body) => {
   const role = localStorage.getItem("role");
   const ut = JSON.parse(localStorage.getItem("user"))?.token;
   console.log("body", body, "token", ut);
-  const res = await fetch(url, {
+  const res = await fetch(`https://s3-y-api-s3y-service.vercel.app/${url}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${
-        JSON.parse(localStorage.getItem("user"))?.token
-      }`,
+      Authorization: `Bearer ${ut}`,
       role: role,
     },
     body,
@@ -28,33 +26,19 @@ export const postRequestWithFiles = async (url, body) => {
   let headers = {
     Authorization: `Bearer ${ut}`,
     role: role,
-    //"Content-Type": "multipart/form-data",
   };
 
-  // Only set Content-Type header if the body is not FormData
   if (!(body instanceof FormData)) {
     headers["Content-Type"] = "application/json";
     body = JSON.stringify(body);
   }
 
-  const res = await fetch(url, {
+  const res = await fetch(`https://s3-y-api-s3y-service.vercel.app/${url}`, {
     method: "POST",
     headers: headers,
     body: body,
   });
-  // const res = await fetch(url, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "multipart/form-data",
-  //     Authorization: `Bearer ${
-  //       JSON.parse(localStorage.getItem("user"))?.token
-  //     }`,
-  //     role: role,
-  //   },
-  //   body,
-  // });
   const data = await res.json();
-  //console.log("data", data, data.data);
 
   if (!res.ok) {
     console.log(data.message);
@@ -66,8 +50,7 @@ export const postRequestWithFiles = async (url, body) => {
 export const getRequest = async (url) => {
   const role = localStorage.getItem("role");
 
-  const res = await fetch(url, {
-    //mode: "no-cors",
+  const res = await fetch(`https://s3-y-api-s3y-service.vercel.app/${url}`, {
     headers: {
       Authorization: `Bearer ${
         JSON.parse(localStorage.getItem("user"))?.token
